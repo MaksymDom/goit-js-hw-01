d5b42324b9d872bbd838515207bc7353d49cba15
Maksym <domitrashchuk.m@gmail.com>
2023-10-15 20:23:07 +0200

    hwjs_3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth(content, padding, border) {
-    const contentWidth = parseInt(content);
-    const paddingWidth = parseInt(padding);
-    const borderThickness = parseInt(border);
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderThickness = parseFloat(border);
     
     return contentWidth + 2 * paddingWidth + 2 * borderThickness;
 }
